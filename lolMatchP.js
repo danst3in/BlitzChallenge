6e82ddd236a8c77cea969c8f0b9ecdaa9e359886
danst3in <50937807+danst3in@users.noreply.github.com>
2020-06-26 19:00:21 -0700

    fs stream functioning per match with 25% decrease in performance.

--- lolMatchP.js
+++ lolMatchP.js
@@ -24,106 +24,167 @@
 }
  */
 
-const lolData = require("./matches.json");
+// const lolData = require("./matches.json");
+const fs = require("fs");
 
+// let lolData = fs.createReadStream("./matches.json", {
+//   flags: "r",
+//   encoding: "utf-8",
+// });
+// let buff = "";
+
+// lolData.on("data", (d) => {
+//   buff += d.toString();
+//   console.log("typeof buff: buff", typeof buff, ": ", buff);
+//   pump();
+// });
+
+function pump() {
+  let pointer =
+    Math.min(buff.indexOf(",0]"), buff.indexOf(",1]")) !== -1
+      ? Math.min(buff.indexOf(",0]"), buff.indexOf(",1]"))
+      : Math.max(buff.indexOf(",0]"), buff.indexOf(",1]"));
+
+  while (pointer >= 0) {
+    pointer += 3;
+    // console.log("pump -> buff[0]", buff[0]);
+    // if (buff[0] === ",") {
+    //   buff = buff.slice(1);
+    // }
+    processLine(buff.slice(0, pointer));
+    buff = buff.slice(pointer + 1);
+    if (buff[0] === ",") {
+      buff = buff.slice(1);
+    }
+    pointer =
+      Math.min(buff.indexOf(",0]"), buff.indexOf(",1]")) !== -1
+        ? Math.min(buff.indexOf(",0]"), buff.indexOf(",1]"))
+        : Math.max(buff.indexOf(",0]"), buff.indexOf(",1]"));
+  }
+}
+
+function processLine(line) {
+  if (line.indexOf("[[[") === 0) {
+    line = line.slice(1);
+  }
+  if (line.indexOf("]]") === [line.length - 2]) {
+    line = line.slice(0, -1);
+  }
+  if (line[line.length - 1] === ",") {
+    line = line.slice(0, line.length - 1);
+  }
+  if (line[0] === ",") {
+    line = line.slice(1);
+  }
+  if (line.length > 0) {
+    // console.log("processLine -> line", line);
+    let battleArr = JSON.parse(line);
+    // console.log("processLine -> battleArr", battleArr);
+    lolBattleTest.mineData(battleArr);
+  }
+}
 class LolProbability {
+  constructor() {
+    (this.statsObj = {}), (this.genStats = {});
+  }
   // receives Input: array of arrays, returns Output: Stats Object
   // Runtime ~O(n*2m^2) ~~O(n*k)
   mineData(arr) {
     if (!arr) {
       throw new Error("Dataset contains undefined matches");
     }
-    const statsObj = {};
-    const length = arr.length;
-    console.log("mineData -> length", length);
+    // const statsObj = {};
+    // const length = arr.length;
+    // console.log("mineData -> length", length);
 
     const updateOpp = (champion, oppChamp, win = true) => {
-      if (statsObj[champion][oppChamp] === undefined) {
-        statsObj[champion][oppChamp] = { win: 0, loss: 0, winP: 0 };
+      if (this.statsObj[champion][oppChamp] === undefined) {
+        this.statsObj[champion][oppChamp] = { win: 0, loss: 0, winP: 0 };
       }
       if (win) {
-        statsObj[champion][oppChamp].win++;
-        statsObj[champion][oppChamp].winP =
-          statsObj[champion][oppChamp].win /
-          (statsObj[champion][oppChamp].win +
-            statsObj[champion][oppChamp].loss);
+        this.statsObj[champion][oppChamp].win++;
+        this.statsObj[champion][oppChamp].winP =
+          this.statsObj[champion][oppChamp].win /
+          (this.statsObj[champion][oppChamp].win +
+            this.statsObj[champion][oppChamp].loss);
       } else {
-        statsObj[champion][oppChamp].loss++;
-        statsObj[champion][oppChamp].winP =
-          statsObj[champion][oppChamp].win /
-          (statsObj[champion][oppChamp].win +
-            statsObj[champion][oppChamp].loss);
+        this.statsObj[champion][oppChamp].loss++;
+        this.statsObj[champion][oppChamp].winP =
+          this.statsObj[champion][oppChamp].win /
+          (this.statsObj[champion][oppChamp].win +
+            this.statsObj[champion][oppChamp].loss);
       }
     };
 
     const updateChamp = (champion, oppTeam, win = true) => {
-      if (statsObj[champion] === undefined) {
-        statsObj[champion] = { win: 0, loss: 0, winP: 0 };
+      if (this.statsObj[champion] === undefined) {
+        this.statsObj[champion] = { win: 0, loss: 0, winP: 0 };
       }
 
       if (win) {
-        statsObj[champion].win++;
-        statsObj[champion].winP =
-          statsObj[champion].win /
-          (statsObj[champion].win + statsObj[champion].loss);
+        this.statsObj[champion].win++;
+        this.statsObj[champion].winP =
+          this.statsObj[champion].win /
+          (this.statsObj[champion].win + this.statsObj[champion].loss);
         oppTeam.forEach((opponent) => updateOpp(champion, opponent));
       } else {
-        statsObj[champion].loss++;
-        statsObj[champion].winP =
-          statsObj[champion].win /
-          (statsObj[champion].win + statsObj[champion].loss);
+        this.statsObj[champion].loss++;
+        this.statsObj[champion].winP =
+          this.statsObj[champion].win /
+          (this.statsObj[champion].win + this.statsObj[champion].loss);
         oppTeam.forEach((opponent) => updateOpp(champion, opponent, false));
       }
     };
 
-    arr.forEach((match, i) => {
-      if (match.length < 3) {
-        console.error(`Match #${i + 1} - is missing data.`);
-      } else {
-        let champOne = match[0];
-        let champTwo = match[1];
+    if (arr.length < 3) {
+      console.error(`Match #${i + 1} - is missing data.`);
+    } else {
+      let champOne = arr[0];
+      let champTwo = arr[1];
 
-        if (match[2] === 1) {
-          [champOne, champTwo] = [champTwo, champOne];
-        }
-
-        champOne.forEach((champ) => updateChamp(champ, champTwo));
-        champTwo.forEach((champ) => updateChamp(champ, champOne, false));
+      if (arr[2] === 1) {
+        [champOne, champTwo] = [champTwo, champOne];
       }
-    });
-    return statsObj;
+
+      champOne.forEach((champ) => updateChamp(champ, champTwo));
+      champTwo.forEach((champ) => updateChamp(champ, champOne, false));
+    }
+
+    return this.statsObj;
   }
 
   // Runtime O(3n)
-  runChampStats(champStats) {
-    let meanSum = 0;
-    let meanP = 0;
-    let variance = 0;
-    let stdDev = 0;
-    let champCount = 0;
+  runChampStats() {
+    this.genStats.meanSum = 0;
+    this.genStats.meanP = 0;
+    this.genStats.variance = 0;
+    this.genStats.stdDev = 0;
+    this.genStats.champCount = 0;
     // champStats.deviants = {};
 
-    for (const champ of Object.values(champStats)) {
-      meanSum += champ.winP;
-      champCount++;
+    for (const champ of Object.values(this.statsObj)) {
+      this.genStats.meanSum += champ.winP;
+      this.genStats.champCount++;
     }
 
-    console.log("runChampStats -> champCount", champCount);
-    console.log("runChampStats -> meanSum", meanSum);
-    meanP = meanSum / champCount;
-    console.log("runChampStats -> meanP", meanP);
+    console.log("runChampStats -> champCount", this.genStats.champCount);
+    console.log("runChampStats -> meanSum", this.genStats.meanSum);
+    this.genStats.meanP = this.genStats.meanSum / this.genStats.champCount;
+    console.log("runChampStats -> meanP", this.genStats.meanP);
 
-    for (const champ of Object.values(champStats)) {
-      variance += (champ.winP - meanP) * (champ.winP - meanP);
+    for (const champ of Object.values(this.statsObj)) {
+      this.genStats.variance +=
+        (champ.winP - this.genStats.meanP) * (champ.winP - this.genStats.meanP);
     }
 
-    variance = variance / champCount;
-    console.log("runChampStats -> variance", variance);
-    stdDev = Math.sqrt(variance);
-    console.log("runChampStats -> stdDev", stdDev);
+    this.genStats.variance = this.genStats.variance / this.genStats.champCount;
+    console.log("runChampStats -> variance", this.genStats.variance);
+    this.genStats.stdDev = Math.sqrt(this.genStats.variance);
+    console.log("runChampStats -> stdDev", this.genStats.stdDev);
 
-    for (const champ of Object.values(champStats)) {
-      champ.deviations = (champ.winP - meanP) / stdDev;
+    for (const champ of Object.values(this.statsObj)) {
+      champ.deviations =
+        (champ.winP - this.genStats.meanP) / this.genStats.stdDev;
       champ.deviant =
         champ.deviations > 1 ? true : champ.deviations < -1 ? true : false;
       if (champ.deviant === true) {
@@ -134,7 +195,7 @@ class LolProbability {
       }
     }
 
-    return champStats;
+    return this.statsObj;
   }
 
   // Prob Formula Source - https://sabr.org/journal/article/probabilities-of-victory-in-head-to-head-team-matchups/
@@ -150,9 +211,10 @@ class LolProbability {
   // P(WPA|WPB) = WPA*(1 - WPB) / WPA*(1 - WPB)  + WPB*(1 - WPA) // PA = winning%AvsB *(loss%BvsA) / PNoDraw
 
   // Runtime O(2m)  ~O(k)
-  computePVals(statObj, champion, oppArr) {
+  computePVals(statsObj, champion, oppArr) {
+    // console.log("LolProbability -> computePVals -> statsObj", statsObj);
     const champProbObj = {
-      pVal: statObj[champion].winP,
+      pVal: statsObj[champion].winP,
       cProb: {},
       teamProb: 0,
     };
@@ -161,14 +223,14 @@ class LolProbability {
     let oppProbs = [];
     oppArr.forEach((opp, i) => {
       console.log(
-        "LolProbability -> computePVals -> statObj[champion][opp].winP",
-        statObj[champion][opp].winP
+        "LolProbability -> computePVals -> statsObj[champion][opp].winP",
+        statsObj[champion][opp].winP
       );
 
       // WPA    // win% vs particular opponent
-      champProbs[i] = statObj[champion][opp].winP;
+      champProbs[i] = statsObj[champion][opp].winP;
       // WP(B)  // opp win% vs vs particular champion
-      oppProbs[i] = statObj[opp][champion].winP;
+      oppProbs[i] = statsObj[opp][champion].winP;
     });
 
     // Secondary Challenge
@@ -191,29 +253,59 @@ class LolProbability {
 
 // initialize test class object
 const lolBattleTest = new LolProbability();
-//  mine json data file
-const lolBattleStats = lolBattleTest.mineData(lolData);
+// //  mine json data file
+let lolData = fs.createReadStream("./matches.json", {
+  flags: "r",
+  encoding: "utf-8",
+});
+let buff = "";
+
+lolData.on("data", (d) => {
+  buff += d.toString();
+  // console.log("typeof buff: buff", typeof buff, ": ", buff);
+  pump();
+});
+lolData.on("error", (err) => {
+  console.error(err);
+});
+
+let lolChampStats;
+
+lolData.on("end", () => {
+  lolChampStats = lolBattleTest.runChampStats();
+  console.log("lolBattleTest.genStats: ", lolBattleTest.genStats);
+  lolBattleTest.computePVals(lolBattleTest.statsObj, "Gangplank", [
+    "Taric",
+    "Fiddlesticks",
+    "Rakan",
+    "Warwick",
+    "Sett",
+  ]);
+});
+// const lolBattleStats = lolBattleTest.mineData(lolData);
 // console.log("typeof lolBattleStats", typeof lolBattleStats);
 
+// compute general statistics for a given mined battle data object
+// const lolChampStats = lolBattleTest.runChampStats();
+// console.log("lolChampStats", lolChampStats);
+// console.log("lolBattleTest.genStats: ", lolBattleTest.genStats);
+
 // compute p values for a given Mined Battle Stats: object, Champion: string, Opposing Team [string:5]
-const lolPValTest = lolBattleTest.computePVals(lolBattleStats, "Gangplank", [
-  "Taric",
-  "Fiddlesticks",
-  "Rakan",
-  "Warwick",
-  "Sett",
-]);
+// const lolPValTest = lolBattleTest.computePVals(lolBattleTest.statsObj,"Gangplank", [
+//   "Taric",
+//   "Fiddlesticks",
+//   "Rakan",
+//   "Warwick",
+//   "Sett",
+// ]);
 
 // additional test
-console.log(
-  lolBattleTest.computePVals(lolBattleStats, "Leblanc", [
-    "Braum",
-    "Kayle",
-    "Anivia",
-    "Quinn",
-    "Mordekaiser",
-  ])
-);
-// compute general statistics for a given mined battle data object
-const lolChampStats = lolBattleTest.runChampStats(lolBattleStats);
-// console.log("lolChampStats", lolChampStats);
+// console.log(
+//   lolBattleTest.computePVals(lolBattleTest.statsObj, "Leblanc", [
+//     "Braum",
+//     "Kayle",
+//     "Anivia",
+//     "Quinn",
+//     "Mordekaiser",
+//   ])
+// );
